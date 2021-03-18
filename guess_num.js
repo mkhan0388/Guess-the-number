cf62f83129450976d361c763b96ee8f7ebb668f4
mkhan0388 <79087325+mkhan0388@users.noreply.github.com>
2021-03-18 20:38:21 +0000

    created the files and added the code
    added the js code to the script file.

---
+++ guess_num.js
@@ -0,0 +1,26 @@
+let maximum = parseInt(prompt('Enter a maximum number'));
+while (!maximum) {
+    maximum = parseInt(prompt('Come on be serious'));
+}
+const targetNum = Math.floor(Math.random * maximum) + 1;
+
+let guess = parseInt(prompt('Enter your first guess'));
+let attempts = 1;
+
+while (parseInt(guess !== targetNum)); {
+    if (guess === 'q') 
+    attempts++;
+    if (guess > targetNum) {
+        guess = prompt('Too high, come down')
+    } else { 
+        guess = prompt('Too low, go back up')
+    }
+
+}
+
+if (guess === 'q') {
+    document.write("You BIG BABY!!")
+} else {
+    document.write("CONGRATS YOU WIN!")
+    document.write(`Well Done, it took ${attempts} guesses`)
+}
